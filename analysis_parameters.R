847c48e878e770bbb447aa0d856ef66d0d4575f5
Arne <arne.thiemann@geo.hu-berlin.de>
2018-12-18 16:44:46 +0100

    Parameter settings file for the analysis part added

---
+++ analysis_parameters.R
@@ -0,0 +1,9 @@
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Project: TopoCliF
+# Script purpose: Setting analysis parameters
+# Date: Mo Dez 18, 2018
+# Author: Arne Thiemann
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+
+ela_assumed <- 1/3 # height of the glacier (0 = lowest value, .5 = middle elevation, 1 = max elevation)
