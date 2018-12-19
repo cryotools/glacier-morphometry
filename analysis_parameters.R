19c150ee196fed2b7442bc6263e096fe8ab24ae3
Arne <arne.thiemann@geo.hu-berlin.de>
2018-12-19 20:29:21 +0100

    Changed plot process to ggplot-based for the raster maps

--- analysis_parameters.R
+++ analysis_parameters.R
@@ -10,3 +10,7 @@ ela_assumed <- 1/3
 
 # number of hexbins at the x-axis (elevation) in the hexbin plot
 nhexbins <- 30
+
+# class breaks for slope analysis in degrees; min and max must be included
+slope_bins <- c(0, .5, 1, 1.5, 5, 10, 20, 30, 45, 60, 90)
+
--- analyze.R
+++ analyze.R
@@ -11,7 +11,9 @@
 options(stringsAsFactors = F)
 
 lapply(c("rgdal", "raster", "rasterVis", "ggplot2", "RColorBrewer", "rgeos", 
-         "spex"), require, character.only = T)
+         "tidyr", "dplyr", "ggthemes", "scales", "grid", "viridis", 
+         "gridExtra", "hexbin"
+         ), require, character.only = T)
 
 source("analysis_parameters.R")
 
@@ -59,7 +61,7 @@ png(
   filename = paste0(
     "/data/projects/topoclif/result-data/run003_graphics/",
     index$RGI_alias[i],
-    "_01_overview.png"
+    "_01_overview2.png"
   ),
   width = 1920,
   height = 1200,
@@ -67,69 +69,82 @@ png(
   res = 120
 )
 
+# calculate ELA height from defined assumption
+ela_calculated <- minValue(temp_ras[["elev_absolute"]]) +
+  (maxValue(temp_ras[["elev_absolute"]]) - 
+     minValue(temp_ras[["elev_absolute"]])) * ela_assumed
 
-# get polygon marking the relevant area
-#above_ela_func <- function(x) ifelse((x >= ela_assumed*1000) == 1, 1, NA)
-below_ela_func <- function(x) ifelse((x <= ela_assumed*1000) == 1, 1, NA)
-#above_ela_poly <- rasterToPolygons(
-#  calc(temp_ras$elev_relative, above_ela_func), 
-#  dissolve = T
-#)
-#TEST <- union(above_ela_poly)
-#above_ela_poly <- unionSpatialPolygons(above_ela_poly, rep(1, length(above_ela_poly)))
+# elevation
+ggras_elev_absolute <- temp_ras[["elev_absolute"]] %>% 
+  as(., "SpatialPixelsDataFrame") %>% 
+  as.data.frame()
+colnames(ggras_elev_absolute) <- c("value", "x", "y")
 
-#above_ela_poly <- qm_rasterToPolygons(calc(temp_ras$elev_relative, above_ela_func))
+ggp_elev <- ggplot() +
+  geom_raster(data=ggras_elev_absolute, aes(x=x, y=y, fill=value)) +
+  coord_fixed() +
+  #theme_map() +
+  theme_light()+
+  #coord_map("albers", lat0 = 39, lat1 = 45) +
+  geom_contour(data=ggras_elev_absolute, aes(x=x, y=y, z=value), breaks = ela_calculated, col = "black")+
+  labs(fill = "Elevation [m a.s.l.]") +
+  theme(
+    legend.position="bottom",
+    legend.key.width=unit(1.5, "cm"),
+    axis.text.y = element_text(angle = 90, hjust = 1),
+    axis.title.x=element_blank(),
+    axis.title.y=element_blank()
+  ) +
+  scale_fill_gradientn(colors = colors_elevation(35))
 
-below_ela_poly <- rasterToPolygons(calc(temp_ras$elev_relative, below_ela_func),
-                              dissolve = T)
 
-# plot
+# slope
+ggras_slope <- temp_ras[["slope"]] %>% 
+  as(., "SpatialPixelsDataFrame") %>% 
+  as.data.frame()
+colnames(ggras_slope) <- c("value", "x", "y")
 
-plot_elevation <- levelplot(
-  temp_ras[["elev_absolute"]],
-  margin=FALSE,
-  colorkey=list(
-    space='bottom',
-    labels=list(at=round(seq(
-      minValue(temp_ras[["elev_absolute"]]),
-      maxValue(temp_ras[["elev_absolute"]]),
-      length.out = 5
-    )), font=4)
-  ),
-  scales=list(y=list(rot=90)),
-  #scales=list(draw=FALSE),            # suppress axis labels
-  col.regions=colors_elevation,                   # color ramp
-  at=seq(minValue(temp_ras[["elev_absolute"]]),
-         maxValue(temp_ras[["elev_absolute"]]), len=110)
-) + latticeExtra::layer(sp.polygons(below_ela_poly, lwd=0, 
-                                    #col="white", 
-                                    fill = rgb(0,0,0,.4)))
-
-plot_slope <- levelplot(
-  temp_ras[["slope"]],
-  margin=FALSE,
-  colorkey=list(
-    space='bottom',
-    labels=list(at= c(0, .5, 1, 1.5, 5, 10, 20, 30, 45, 60, 90), font=4)
-  ),
-
-  scales=list(y=list(rot=90)),
-  #scales=list(draw=FALSE),            # suppress axis labels
-  col.regions=colors_slope,                   # color ramp
-  at=c(0, .5, 1, 1.5, 5, 10, 20, 30, 45, 60, 90)
-) + #latticeExtra::layer(sp.polygons(above_ela_poly, lwd=0.8, col='black'))
-  latticeExtra::layer(sp.polygons(below_ela_poly, lwd=0, 
-                                  #col="white", 
-                                  fill = rgb(0,0,0,.4)))
-
-print(
-  plot_elevation,
-  position=c(0, 0, .5, 1), more = T
+# classify slope raster
+ggras_slope$value_class <- cut(
+  ggras_slope$value, 
+  breaks = slope_bins,
+  include.lowest = T, 
+  ordered_result = T
 )
-print(
-  plot_slope, 
-  position=c(.5, 0, 1, 1)
-)
+
+
+
+ggp_slope <- ggplot() +
+  geom_raster(data=ggras_slope, aes(x=x, y=y, fill=value_class)) +
+  coord_fixed() +
+  #theme_map() +
+  theme_light()+
+  #coord_map("albers", lat0 = 39, lat1 = 45) +
+  geom_contour(data=ggras_elev_absolute, aes(x=x, y=y, z=value), breaks = ela_calculated, col = "black")+
+  labs(fill = "Slope [°]") +
+  theme(
+    legend.position="bottom",
+    #legend.key.width=unit(.2, "cm"),
+    axis.text.y = element_text(angle = 90, hjust = 1),
+    axis.title.x=element_blank(),
+    axis.title.y=element_blank(), 
+    legend.direction = "horizontal", 
+    legend.box = "horizontal"
+  ) +
+  guides(
+    fill = guide_legend(
+      label.position = "bottom",
+      nrow = 1,
+      byrow = T
+    )
+  ) +
+  scale_fill_manual(values = c("dodgerblue4", "dodgerblue3", "dodgerblue", 
+                      "deepskyblue", "khaki", "yellow", "orange", 
+                      "red2", "red3", "red4")) +
+  xlim(layer_scales(ggp_elev)$x$range$range) +
+  ylim(layer_scales(ggp_elev)$y$range$range)
+  
+grid.arrange(ggp_elev, ggp_slope, ncol = 2)
 
 dev.off()
 
@@ -152,7 +167,7 @@ ggplot(
       #alpha = ..count.., 
       fill = ..count..)
   ) + 
-  geom_smooth() +
+  #geom_smooth() +
   theme_light() +
   coord_fixed(ratio = ((
     max(temp_ras_data$elev_absolute) - min(temp_ras_data$elev_absolute) 
@@ -173,4 +188,8 @@ ggsave(
 
 setTxtProgressBar(pb, i)
 }
+
+writeLines("\n")
+
+
 
