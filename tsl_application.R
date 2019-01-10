1163eeb1f64e791f362d5a7d7daaac09ffd0ae8d
Arne <arne.thiemann@geo.hu-berlin.de>
2019-01-10 17:57:53 +0100

    TSL script and plots updated

--- tsl_application.R
+++ tsl_application.R
@@ -1,48 +1,25 @@
+require(tidyverse)
+
+source("analysis_parameters.R")
+
+index <- read.csv(
+  #file.choose(),
+  index_input_path,
+  sep = ";"
+)
+
 TSL_input <- read.csv(
-  "../../input-data/tables/TSL+RGI.csv"
+  "/data/projects/topoclif/input-data/tables/TSL+RGI.csv"
 )
 
 TSL_input <- TSL_input[TSL_input$RGIId %in% index$RGI_ID,]
 length(unique(TSL_input$RGIId))
 
-# plot
 ggplot(TSL_input) +
   geom_line(
     aes(
-      x = as.POSIXct(strptime(TSL_input$BgnDate, "%Y%m%d")),
-      y = TSL_ELEV,
-      col = RGIId
+      x = as.POSIXct(LS_DATE),
+      y = TSL_ELEV
     )
-  )
-
-  # plot
-  ggplot(TSL_input) +
-    geom_line(
-      aes(
-        x = as.POSIXct(strptime(TSL_input$BgnDate, "%Y%m%d")),
-        y = TSL_ELEV
-      )
-    ) +
+  ) +
   facet_wrap(~ RGIId)
-
-  
-  # plot
-  ggplot(TSL_input) +
-    geom_line(
-      aes(
-        x = as.POSIXct(LS_DATE),
-        y = TSL_ELEV,
-        col = RGIId
-      )
-    )
-  
-  # plot
-  ggplot(TSL_input) +
-    geom_line(
-      aes(
-        x = as.POSIXct(LS_DATE),
-        y = TSL_ELEV
-      )
-    ) +
-    facet_wrap(~ RGIId)
-  
